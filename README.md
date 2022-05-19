8ba7463adbc9f9b7ac15b25159a2eedea3ecf0d8
ragnar <ragnar@rmgk.de>
2022-05-20 00:45:28 +0200

    should have added stuff to git sooner ...

---
+++ README.md
@@ -0,0 +1,48 @@
+# My Gluon Application
+
+This Gluon sample was generated from https://start.gluon.io
+
+## Basic Requirements
+
+A list of the basic requirements can be found online in the [Gluon documentation](https://docs.gluonhq.com/#_requirements).
+
+## Quick instructions
+
+### Run the sample on JVM/HotSpot:
+
+    mvn gluonfx:run
+
+### Run the sample as a native image:
+
+    mvn gluonfx:build gluonfx:nativerun
+
+### Run the sample as a native android image:
+
+    mvn -Pandroid gluonfx:build gluonfx:package gluonfx:install gluonfx:nativerun
+
+### Run the sample as a native iOS image:
+
+    mvn -Pios gluonfx:build gluonfx:package gluonfx:install gluonfx:nativerun
+
+## Selected features
+
+This is a list of all the features that were selected when creating the sample:
+
+### JavaFX 18 Modules
+
+ - javafx-base
+ - javafx-graphics
+ - javafx-controls
+
+### Gluon Features
+
+ - Glisten: build platform independent user interfaces
+ - Attach ble
+ - Attach connectivity
+ - Attach display
+ - Attach lifecycle
+ - Attach pictures
+ - Attach position
+ - Attach push notifications
+ - Attach statusbar
+ - Attach storage
---
+++ src/main/scala/de/rmgk/sample/Main.scala
@@ -0,0 +1,71 @@
+package de.rmgk.sample
+
+import com.gluonhq.attach.display.DisplayService
+import com.gluonhq.attach.util.Platform
+import com.gluonhq.charm.glisten.application.AppManager
+import com.gluonhq.charm.glisten.control.AppBar
+import com.gluonhq.charm.glisten.control.FloatingActionButton
+import com.gluonhq.charm.glisten.mvc.View
+import com.gluonhq.charm.glisten.visual.MaterialDesignIcon
+import com.gluonhq.charm.glisten.visual.Swatch
+import javafx.application.Application
+import javafx.geometry.Dimension2D
+import javafx.geometry.Pos
+import javafx.scene.Scene
+import javafx.scene.control.Label
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
+import javafx.scene.layout.VBox
+import javafx.stage.Stage
+import com.gluonhq.charm.glisten.application.AppManager.HOME_VIEW
+import javafx.event.ActionEvent
+import rescala.default._
+
+object Main:
+  def main(args: Array[String]): Unit = { Application.launch(classOf[Main], args: _*) }
+
+class Main extends Application {
+  final private val appManager = AppManager.initialize(this.postInit)
+  override def init(): Unit = {
+
+    val evt = Evt[String]()
+    evt.observe(println)
+
+    appManager.addViewFactory(
+      HOME_VIEW,
+      () => {
+        def foo() = {
+          val fab =
+            new FloatingActionButton(
+              MaterialDesignIcon.SEARCH.text,
+              (e: ActionEvent) => evt.fire(s"hahah!")
+            )
+          val imageView = new ImageView(new Image(classOf[Main].getResourceAsStream("openduke.png")))
+          imageView.setFitHeight(200)
+          imageView.setPreserveRatio(true)
+          val label = new Label("Hello, My Gluon Application!")
+          val root  = new VBox(20, imageView, label)
+          root.setAlignment(Pos.CENTER)
+          val view = new View(root) {
+            override protected def updateAppBar(appBar: AppBar): Unit = { appBar.setTitleText("My Gluon Application") }
+          }
+          fab.showOn(view)
+          view
+        }
+        foo()
+      }
+    )
+  }
+  override def start(stage: Stage): Unit = { appManager.start(stage) }
+  private def postInit(scene: Scene): Unit = {
+    Swatch.LIGHT_GREEN.assignTo(scene)
+    scene.getStylesheets.add(classOf[Main].getResource("styles.css").toExternalForm)
+    scene.getWindow.asInstanceOf[Stage].getIcons.add(new Image(classOf[Main].getResourceAsStream("/icon.png")))
+    if (Platform.isDesktop) {
+      val dimension2D =
+        DisplayService.create.map(_.getDefaultDimensions()).orElse(new Dimension2D(640, 480))
+      scene.getWindow.setWidth(dimension2D.getWidth)
+      scene.getWindow.setHeight(dimension2D.getHeight)
+    }
+  }
+}
